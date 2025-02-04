2ac323b0d6bc36e996273e3845b8b1604ec5f3fd
peet <teilzeitjobbewerber@gmx.de>
2025-02-04 19:52:54 +0100

    Feat: Checkbox changed from .png change to html native checkbox

--- js/login.js
+++ js/login.js
@@ -7,77 +7,6 @@ import {
 } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-auth.js";
 
 
-let checked = false;
-let isVisible = false;
-let userData;
-let emailInputField = document.getElementById(`email`);
-let passwordInputField = document.getElementById(`password`);
-let userInitals = null;
-
-
-async function logInInit() {
-}
-
-
-/**
- * Executes on clicking the login button.
- */
-function login() {
-  let email = document.getElementById("email");
-  emailCheck(email, password);
-}
-
-
-function checkMailPw(email, password) {
-}
-
-/**
- * Checks the rightness of  email password combination. 
- * Logs in the user. 
- */
-function emailCheck(email, password) {
-  let emailFound = false;
-  let emailFoundIndex = 0;
-  for (let i = 0; i < userData.length; i++) {
-    let user = userData[i];
-    if (user["email"] === email.value) {
-      emailFound = true;
-      emailFoundIndex = i;
-    }
-  }
-  if (emailFound && userData[emailFoundIndex]["password"] === password.value) {
-    document.querySelector(`.login-area`).reset()
-    positiveLogin(emailFoundIndex)
-  }
-}
-
-
-/**
- * Gets the name and the initlas of a user that have been logged in. 
- * Redirects to summary.html 
- */
-function positiveLogin(i) {
-  userName = userData[i]["name"];
-  userInitals = userData[i]["initials"];
-  let loggedInUser = {
-    "name": userName,
-    "initials": userInitals
-  };
-  setArray("loggedInUser", loggedInUser);
-  window.location.href = "./summary.html";
-}
-
-/**
- * Handles the log in of a guest user.
- */
-function guestLoginIn() {
-  let loggedInUser = {
-    "name": "Guest",
-    "initials": "GU"
-  };
-  setArray("loggedInUser", loggedInUser);
-  window.location.href = "./summary.html";
-}
 
 
 /**
@@ -98,33 +27,6 @@ function togglePasswordVisibility() {
   }
 }
 
-logSth = ()=>{
-  console.log("I am here");
-} 
 
-/**
- * Changes visibility of a checkmark png. 
- * From unchecked to checked and vice versa. 
- */
-function changeRectangle() {
-  let checkedRectangle = document.getElementById("rectangle");
 
-  if (checked) {
-    checkedRectangle.src = "./assets/img/log_in/not-checked.png";
-    checked = false;
-  } else {
-    checkedRectangle.src = "./assets/img/log_in/checked.png";
-    checked = true;
-  }
-}
-
-let pwInput = document.getElementById('password');
-
-function pwCheck() {
-
-  if (pwInput.value != pwInputRepeat.value) {
-    pwInput.setCustomValidity('Password Must be Matching.');
-  } else {
-    pwInput.setCustomValidity('');
-  }
-}
+// ------------------------- DOM REFERENCES -------------------------
